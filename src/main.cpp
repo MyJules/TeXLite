68249d8d9df2f18ae1b77b78e5eb7fb5f3e11d03
MyJules <mudryk200@gmail.com>
2023-06-30 21:57:53 +0200

    icon

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <syntaxhighlihgter.h>
 #include <textcharformat.h>
 
+#include <QIcon>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QtQml/qqmlregistration.h>
@@ -14,6 +15,7 @@ int main(int argc, char *argv[])
     TempFolderGuard tempfolder(QDir::currentPath(), "temp");
 
     QGuiApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/icons/imgs/icon.png"));
 
     qmlRegisterType<TexEngine>("com.tex", 1, 0, "TexEngine");
     qmlRegisterType<FileSystem>("com.file", 1, 0, "FileSystem");
